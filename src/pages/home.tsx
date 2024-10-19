8b2e6bc5cb31a4e296bac2e9cd2a42f95d3ee671
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-10-19 20:55:57 +0200

    venues page

---
+++ src/pages/home.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+type Accommodation = {
+    id: string;
+    name: string;
+    description: string;
+    media: {
+        url: string;
+        alt: string;
+    }[];
+    price: number;
+    maxGuests: number;
+    rating: number;
+    created: string;
+    updated: string;
+    meta: {
+        wifi: boolean;
+        parking: boolean;
+        breakfast: boolean;
+        pets: boolean;
+    };
+    location: {
+        address: string;
+        city: string;
+        zip: string;
+        country: string;
+        continent: string;
+        lat: number;
+        lng: number;
+    };
+    _count: {
+        bookings: number;
+    };
+};
+
+export function Home() {
+    const [posts, setPosts] = useState<Accommodation[]>([]);
+
+    useEffect(() => {
+        async function getData() {
+            const url = "https://v2.api.noroff.dev/holidaze/venues";
+            const response = await fetch(url);
+            const json = await response.json();
+            const data = json.data;
+            setPosts(data);
+        }
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.name}</h2>
+                    <p>{post.description}</p>
+                    {post.media.length > 0 && (
+                        <img src={post.media[0].url} alt={post.media[0].alt} />
+                    )}
+                    <p>{post.price}</p>
+                    <p>{post.maxGuests}</p>
+                    <p>{post.rating}</p>
+                    {post.meta.wifi && <p>Wifi</p>}
+                    {post.meta.parking && <p>Parking</p>}
+                    {post.meta.breakfast && <p>Breakfast</p>}
+                    {post.meta.pets && <p>Pets</p>}
+                </div>
+            ))}
+        </div>
+    );
+}
