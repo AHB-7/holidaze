027d225c1f07e0280a5bc19088dc08e3aa21b51a
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-11-12 23:14:07 +0100

    logIn form UI

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import {
+    // FormInput,
     LogInContainer,
+    LogOut,
     LowerNav,
     Navbar as NavbarSc,
+    // SubmitBtn,
     UpperNav,
 } from "../../styles/layouts/nav";
 import { MdOutlineMenu } from "react-icons/md";
 import { useState } from "react";
-import { FiLogIn } from "react-icons/fi";
+// import { FiLogIn } from "react-icons/fi";
 
 export function Navbar() {
     const [active, setActive] = useState(false);
@@ -22,19 +25,31 @@ export function Navbar() {
                 <Link to="/">
                     <img src={logo} alt="asa" />
                 </Link>
-                <LogInContainer as={Link} to="/">
+                {/* <LogInContainer as={Link} to="/" onClick={handleClick}>
                     <p>LogIn</p>
                     <FiLogIn stroke="white" />
-                </LogInContainer>
+                </LogInContainer> */}
                 <LogInContainer>
                     <MdOutlineMenu fill="white" onClick={handleClick} />
                 </LogInContainer>
             </UpperNav>
+            {/* {active ? (
+                <LowerNav>
+                    <h2>Login</h2>
+                    <form action="">
+                        <FormInput type="text" placeholder="Email" />
+                        <FormInput type="password" placeholder="Password" />
+                        <SubmitBtn type="submit">Login</SubmitBtn>
+                    </form>
+                </LowerNav>
+            ) : null} */}
             {active ? (
                 <LowerNav>
                     <Link to="/profile">Profile</Link>
                     <Link to="/booling">booking</Link>
-                    <Link to="/logout">Logout</Link>
+                    <LogOut as={Link} to="/logout">
+                        Logout
+                    </LogOut>
                 </LowerNav>
             ) : null}
         </NavbarSc>
--- src/styles/layouts/nav.ts
+++ src/styles/layouts/nav.ts
@@ -2,13 +2,16 @@ import styled from "styled-components";
 
 export const Navbar = styled.nav`
     background-color: ${({ theme }) => theme.colors.text};
-    max-width: 20rem;
+    width: 100%;
+    max-width: 25rem;
     border-radius: 1.2rem;
     padding: 0.5rem 0.7rem;
-    margin: 0.5rem 1rem;
     position: fixed;
-    top: 0;
-    width: 90%;
+    top: 0.5rem;
+    margin: 0 1rem;
+    @media screen and (max-width: 450px) {
+        margin: 0 auto;
+    }
     z-index: 100;
 `;
 export const UpperNav = styled.div`
@@ -25,25 +28,59 @@ export const LowerNav = styled.div`
     font-weight: 900;
     padding: 1rem;
     gap: 1rem;
+    > h2 {
+        color: ${({ theme }) => theme.colors.background};
+    }
     > * {
         color: ${({ theme }) => theme.colors.background};
     }
-    :last-child {
-        font-size: 1rem;
-        justify-self: end;
-        color: red;
-    }
+`;
+export const LogOut = styled.a`
+    font-size: 1rem;
+    justify-self: end;
+    color: red;
 `;
 export const LogInContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 1rem;
+    gap: 0.1rem;
     > p {
         font-size: 1.3rem;
         color: ${({ theme }) => theme.colors.background};
     }
     > svg {
         font-size: 1.7rem;
+    }
+`;
+export const FormInput = styled.input`
+    border: none;
+    border-radius: 0.8rem;
+    border: 0.05rem solid white;
+    padding: 0.6rem;
+    font-size: 1rem;
+    width: 100%;
+    margin: 1rem 0;
+    background-color: transparent;
+    color: ${({ theme }) => theme.colors.background};
+    outline: none;
+    transition: border 0.1s;
+    &:focus {
+        border: 0.1rem solid white;
+    }
+`;
+export const SubmitBtn = styled.button`
+    background-color: ${({ theme }) => theme.colors.background});
+    color: ${({ theme }) => theme.colors.text};
+    border: none;
+    border-radius: 0.8rem;
+    padding: 0.5rem;
+    font-size: 1.2rem;
+    width: 100%;
+    margin: 1rem 0;
+    cursor: pointer;
+    transition: background-color 0.2s;
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.secondary};
     }
 `;
