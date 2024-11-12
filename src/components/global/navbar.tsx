a495ceefff04d1028d3f3d2df9831087f5440092
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-11-12 17:37:48 +0100

    navBar style structure

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
-import { Navbar as NavbarSc } from "../../styles/layouts/nav";
+import {
+    LogInContainer,
+    LowerNav,
+    Navbar as NavbarSc,
+    UpperNav,
+} from "../../styles/layouts/nav";
 import { MdOutlineMenu } from "react-icons/md";
 import { useState } from "react";
+import { FiLogIn } from "react-icons/fi";
 
 export function Navbar() {
     const [active, setActive] = useState(false);
@@ -12,18 +18,24 @@ export function Navbar() {
 
     return (
         <NavbarSc>
-            <div>
+            <UpperNav>
                 <Link to="/">
                     <img src={logo} alt="asa" />
                 </Link>
-                <MdOutlineMenu fill="white" onClick={handleClick} />
-            </div>
+                <LogInContainer as={Link} to="/">
+                    <p>LogIn</p>
+                    <FiLogIn stroke="white" />
+                </LogInContainer>
+                <LogInContainer>
+                    <MdOutlineMenu fill="white" onClick={handleClick} />
+                </LogInContainer>
+            </UpperNav>
             {active ? (
-                <div>
+                <LowerNav>
                     <Link to="/profile">Profile</Link>
                     <Link to="/booling">booking</Link>
                     <Link to="/logout">Logout</Link>
-                </div>
+                </LowerNav>
             ) : null}
         </NavbarSc>
     );
--- src/styles/layouts/nav.ts
+++ src/styles/layouts/nav.ts
@@ -3,34 +3,47 @@ import styled from "styled-components";
 export const Navbar = styled.nav`
     background-color: ${({ theme }) => theme.colors.text};
     max-width: 20rem;
-    border-radius: 2rem;
+    border-radius: 1.2rem;
     padding: 0.5rem 0.7rem;
     margin: 0.5rem 1rem;
     position: fixed;
     top: 0;
     width: 90%;
     z-index: 100;
-    > :nth-child(1) {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        font-size: 2rem;
+`;
+export const UpperNav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const LowerNav = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 2.4rem;
+    font-weight: 900;
+    padding: 1rem;
+    gap: 1rem;
+    > * {
+        color: ${({ theme }) => theme.colors.background};
     }
-    > :nth-child(2) {
-        > * {
-            color: ${({ theme }) => theme.colors.background};
-        }
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        font-size: 3.4rem;
-        padding: 1rem 0.5rem;
-        font-weight: 900;
-        :hover {
-            color: ${({ theme }) => theme.colors.primary};
-        }
-        :active {
-            color: ${({ theme }) => theme.colors.secondary};
-        }
+    :last-child {
+        font-size: 1rem;
+        justify-self: end;
+        color: red;
+    }
+`;
+export const LogInContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    > p {
+        font-size: 1.3rem;
+        color: ${({ theme }) => theme.colors.background};
+    }
+    > svg {
+        font-size: 1.7rem;
     }
 `;
